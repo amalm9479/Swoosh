99f4693dbe5ba0f6cc45a6e24d3c434042503e9a
first1 <amalm9479@gmail.com>
2021-02-17 01:59:57 +0300

    second swoosh update 2

--- app/src/main/java/com/amalm9479/swoosh/MainActivity.kt
+++ app/src/main/java/com/amalm9479/swoosh/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.amalm9479.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        getStartedBtn.setOnClickListener {
+            val leagueIntent=Intent(this, LeagueActivity::class.java)
+            startActivity(leagueIntent)
+        }
 
     }
 }
